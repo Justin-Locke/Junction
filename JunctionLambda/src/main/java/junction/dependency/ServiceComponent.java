d5c78a07520b4a4a0aee24b58b4caf7e62b59c95
Justin Locke <Justindeanlocke@gmail.com>
2024-02-08 19:09:55 -0600

    Added Back end files and dependencies

---
+++ JunctionLambda/src/main/java/junction/dependency/ServiceComponent.java
@@ -0,0 +1,95 @@
+package junction.dependency;
+
+import com.nashss.se.tale.activity.*;
+import dagger.Component;
+
+import javax.inject.Singleton;
+
+@Singleton
+@Component(modules = {DaoModule.class, MetricsModule.class})
+public interface ServiceComponent {
+
+    /**
+     * Method to instantiate Get City.
+     * @return new GetCity().
+     */
+    GetCity provideGetCity();
+
+    /**
+     * Method to instantiate GetAllCities.
+     * @return new GetAllCities().
+     */
+    GetAllCities provideGetAllCities();
+
+    /**
+     * Method to instantiate CreateNewActivity.
+     * @return new CreateNewActivity().
+     */
+    CreateNewActivity provideCreateNewActivity();
+
+    /**
+     * Method to instantiate GetActivity.
+     * @return new GetActivity().
+     */
+    GetActivity provideGetActivity();
+
+    /**
+     * Method to instantiate GetAllActivities.
+     * @return new GetAllActivities().
+     */
+    GetAllActivities provideGetAllActivities();
+
+    /**
+     * Method to instantiate GetPersonalActivities.
+     * @return new GetPersonalActivities().
+     */
+    GetPersonalActivities provideGetPersonalActivities();
+
+    /**
+     * Method to instantiate EditActivity.
+     * @return new EditActivity().
+     */
+    EditActivity provideEditActivity();
+
+    /**
+     * Method to instantiate CreateComment.
+     * @return new CreateComment().
+     */
+    CreateComment provideCreateComment();
+
+    /**
+     * Method to instantiate GetCommentsForActivity.
+     * @return new GetCommentsForActivity(0.
+     */
+    GetCommentsForActivity provideGetCommentsForActivity();
+
+    /**
+     * Method to instantiate GetPersonalComments.
+     * @return new GetPersonalComments().
+     */
+    GetPersonalComments provideGetPersonalComments();
+
+    /**
+     * Method to instantiate GetComment.
+     * @return new GetComment().
+     */
+    GetComment provideGetComment();
+
+    /**
+     * Method to instantiate DeleteComment.
+     * @return new DeleteComment().
+     */
+    DeleteComment provideDeleteComment();
+
+    /**
+     * Method to instantiate DeleteActivity.
+     * @return new DeleteActivity().
+     */
+    DeleteActivity provideDeleteActivity();
+
+    /**
+     * Method to instantiate EditComment.
+     * @return new EditComment().
+     */
+    EditComment provideEditComment();
+}
